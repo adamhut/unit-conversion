21b229365416ca0ec45491b1bd9c58ccb2b19a64
Adam Huang <adamhut@gmail.com>
2020-07-04 05:48:40 -0700

    covert kilogram to lbs

---
+++ src/Weight.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Adamhut\UnitConversion;
+
+class Weight
+{
+    protected $kilograms;
+
+
+    public static function fromKilograms(float $kilograms):self
+    {
+        return (new static($kilograms));
+    }
+
+    public function __construct(float $kilograms)
+    {
+        $this->kilograms = $kilograms;
+    }
+
+
+    public function toLbs() :float
+    {
+
+
+        return $this->kilograms * 2.204623;
+    }
+
+}
---
+++ tests/WeightTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Adamhut\UnitConversion\Tests;
+
+use Adamhut\UnitConversion\Weight;
+use PHPUnit\Framework\TestCase;
+
+class WeightTest extends TestCase
+{
+    /** @test */
+    public function it_can_convert_kilogram_to_lbs()
+    {
+
+        $lbs = Weight::fromKilograms(100)->toLbs();
+
+        $this->assertEquals(220.4623,$lbs);
+    }
+}
